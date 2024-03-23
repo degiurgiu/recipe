e835e614dfa5fbc770f431e22612679c33a8d76a
degiurgiu <giurgiu_dan94@yahoo.com>
2024-03-22 23:00:02 -0400

    Init

---
+++ storage/framework/views/2c762918d9f8226f86b79b182956af43.php
@@ -0,0 +1,12 @@
+<p>Welcome to ,</p>
+
+<p>Please Login with the your E-Mail and Password below to start and complete the new hire kit.</p>
+
+<br>E-Mail: <i><?php echo e($user['email']); ?></i>
+<br>Temporary Password: <i><?php echo e($tmp_password); ?></i>
+<br>URL: <i><a href="<?php echo e(secure_url('/')); ?>"> <?php echo e(secure_url('/')); ?></a></i>
+
+<p><br></p>
+<p>Thank You,
+
+<?php /**PATH F:\recipe_store_laravel\resources\views/email/welcome_new_user.blade.php ENDPATH**/ ?>
